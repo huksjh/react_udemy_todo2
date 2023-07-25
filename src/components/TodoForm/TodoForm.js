3067d5375b5fd994c46976f4400650997ea15521
huks <wkdgur1209@gmail.com>
2023-07-25 23:14:00 +0900

    이것저것 추가

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 import styles from "./TodoForm.module.css";
+import ErrorModal from "../UI/ErrorModal/ErrorModal";
 
 const TodoForm = (props) => {
     // Username 설정
@@ -21,6 +22,7 @@ const TodoForm = (props) => {
         //console.log(userAge.toString().length);
         if (userName.trim().length === 0 || userAge.length === 0) {
             console.log("빈값 있음");
+
             return;
         }
 
@@ -34,6 +36,8 @@ const TodoForm = (props) => {
             name: userName,
             age: userAge,
         };
+
+        // App.js 에서 넘어온 함수(onAddTodo={addTodoHandler})로 입력받은 값을 전달
         props.onAddTodo(submitData);
 
         event.target.reset();
@@ -41,20 +45,23 @@ const TodoForm = (props) => {
         setUserAge("");
     };
     return (
-        <Card>
-            <form onSubmit={formTodoSubmit} className={styles.input}>
-                <div>
-                    <label>UserName</label>
-                    <input type="text" onChange={userNameChangeHandeler} />
-                </div>
-                <div>
-                    <label>Age</label>
-                    <input type="number" onChange={userAgeChangeHandeler} />
-                </div>
+        <>
+            <ErrorModal title="테스트" message="ㅁㅁㅁㅁ" />;
+            <Card>
+                <form onSubmit={formTodoSubmit} className={styles.input}>
+                    <div>
+                        <label>UserName</label>
+                        <input type="text" onChange={userNameChangeHandeler} />
+                    </div>
+                    <div>
+                        <label>Age</label>
+                        <input type="number" onChange={userAgeChangeHandeler} />
+                    </div>
 
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </>
     );
 };
 
